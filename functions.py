52ccc37c149fb629be5d9d855595157691c271e0
Jennifer Colna <jennifer.colna@gmail.com>
2018-09-19 12:26:01 -0500

    find most common word function

--- functions.py
+++ functions.py
@@ -10,39 +10,65 @@ exceptions = {
 
 #format words from a file
 def formatFileText(file):
+
     text = open(file, 'r').read() #read text in the file
     words = text.split(" ") #split text into list of words
     wordList = [] #initialize a new list
+
     for word in words:
         lower = word.lower() #make all words lowercase for more accurate counting
         if lower in exceptions: #dont count top 15 most common words
             wordList = wordList
         else:
             fixedWord = re.findall(r'[a-z]*', lower) #filter out punctuation or whitespace, returns a list with a length of 1
-            wordList.append(fixedWord[0]) #add item in list to the wordList    
+            wordList.append(fixedWord[0]) #add item in list to the wordList  
+
     return wordList
 
 #format words from text input
 def formatInputText(text):
+
     words = text.split(" ")
     wordList = []
+
     for word in words:
         lower = word.lower() #make all words lowercase for more accurate counting
         if lower in exceptions: #dont count top 15 most common words
             wordList = wordList
         else:
             fixedWord = re.findall(r'[a-z]*', lower) #filter out punctuation or whitespace, returns a list with a length of 1
-            wordList.append(fixedWord[0]) #add item in list to the wordList      
+            wordList.append(fixedWord[0]) #add item in list to the wordList  
+
     return wordList        
 
 #build dictionary with word list with counts
-def findWordCount(list){
+def findWordCount(list):
+
     wordCount = {} #initialize empty dictionary, keys will be words, values will be the count
+
     for word in list:
         if word not in wordCount:
             wordCount[word] = 1 #add to dictionary and set count to 1
         else:
             wordCount[word] = wordCount[word] + 1 #if its there, increase the count by one
+
     return wordCount
-}
+
+#find most used word and its count from a dictonary of words and counts and a list of words
+def findMostUsed(dict):
+
+    largestCount = 0 #initalize count and word used most
+    most = ""
+   
+    for word, count in dict.items():
+        if count > largestCount: #change the word and count when a larger count is encountered
+            most = word
+            largestCount = count
+
+    return "The word used the most in this text is "+word+". It was used "+count+" times."
+
+
+def wordBarGraph(dict, list):
+
+ minCount = round(len(list)*0.01) #filters out words used less than one percent of total word count (i.e: for 100 words, words used more than once)
 
